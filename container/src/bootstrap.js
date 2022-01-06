951bfd17116d92070ae58fa79ee7b4b5f63a79e2
kaliayev <kaliayev@protonmail.com>
2022-01-06 18:59:41 +0100

    Init microfrontends with webpack module federation

---
+++ container/src/bootstrap.js
@@ -0,0 +1,4 @@
+import "products/ProductsIndex"; // It points to remotes field from ModuleFederationPlugin config in webpack config
+import "cart/CartShow"; // It points to remotes field from ModuleFederationPlugin config in webpack config
+
+console.log("Container!");
